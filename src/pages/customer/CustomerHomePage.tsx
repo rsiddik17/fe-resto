520697eb2ce089629c94dc28ee99036176551668
rsiddik17 <rsiddik369@gmail.com>
2026-04-05 16:22:07 +0700

    add some utility

--- src/pages/customer/CustomerHomePage.tsx
+++ src/pages/customer/CustomerHomePage.tsx
@@ -1,7 +1,23 @@
+import { useNavigate } from "react-router";
+import { useAuthStore } from "../../store/useAuthStore";
+import Button from "../../components/ui/Button";
+
 const CustomerHomePage = () => {
+  const navigate = useNavigate();
+  const logout = useAuthStore((state) => state.logout);
+
+  const handleLogout = () => {
+    logout();
+
+    navigate("/");
+  };
+
   return (
     <div>
       <h1>Halaman Customer</h1>
+      <Button className="ml-2" onClick={handleLogout}>
+        Keluar
+      </Button>
     </div>
   );
 };
